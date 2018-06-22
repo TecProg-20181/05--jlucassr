59c95007274bb2469a18eb4907007ad553357901
João Lucas Sousa Reis <joao.lucas.ssr@gmail.com>
2018-06-22 13:53:46 -0300

    Adding test_bytes_to_readable

--- diskspace/test_diskspace.py
+++ diskspace/test_diskspace.py
@@ -1,17 +1,36 @@
 import unittest
+import subprocess
+import os
+import diskspace
 
-from diskspace import diskspace
+from diskspace import subprocess_check_output
+from diskspace import bytes_to_readable
+#from diskspace import du_command
 
 class test(unittest.TestCase):
 
+    def setUp(self):
+        self.command = 'du '
+        self.abs_directory = os.path.abspath('.')
+        self.command += self.abs_directory
+        self.path = 'home/teste'
+
     def test_subprocess_check_output(self):
-        abs_directory = os.path.abspath('')
-        cmd = 'du -d 1 ' + abs_directory
+        result = diskspace.subprocess_check_output(self.command)
+        results = subprocess_check_output('du -d 1 {}'.format(self.abs_directory))
 
-        result = subprocess.check_output(cmd.strip().split(' '))
-        command = subprocess_check_output('du -d 1 {}'.format(abs_directory))
+        self.assertEqual(result, results)
 
-        self.assertEqual(command, result)
+    def test_bytes_to_readable(self):
+        blocks = 100
+        result = "50.00Kb"
 
-        suite = unittest.TestLoader().loadTestsFromTestCase(TestDiskspaceMethods)
-        unittest.TextTestRunner(verbosity=2).run(suite)
+        self.assertEqual(bytes_to_readable(blocks), result)
+
+
+        #suite = unittest.TestLoader().loadTestsFromTestCase(TestDiskspaceMethods)
+        #unittest.TextTestRunner(verbosity=2).run(suite)
+
+
+if __name__ == '__main__':
+    unittest.main()
